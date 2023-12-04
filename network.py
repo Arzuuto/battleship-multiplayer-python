d022366ad8763b70e73245246e710dffa0d44c4a
Arnaud Ho Chouck Foune <arnaud.ho-chouck-foune@epitech.eu>
2023-12-05 02:45:12 +0900

    add: add port in parameter

--- network.py
+++ network.py
@@ -2,13 +2,14 @@ import socket
 import sys
 
 class Network:
-    def __init__(self, host='127.0.0.1', port=8086):
+    def __init__(self, host='127.0.0.1', port=8086): #port need to be in the parameter
         self.host = host
         self.port = port
         self.socket = None
         self.client_socket = None
         self.addr = None
 
+    # Host a game
     def start_server(self):
         try:
             self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -21,15 +22,17 @@ class Network:
             print("Error starting server: " + str(e))
             sys.exit(84)
 
-    def connect_to_server(self):
+    # Connect to a host as a client
+    def connect_to_server(self, port):
         try:
             self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            self.socket.connect((self.host, self.port))
-            print("Connected to server at " + str(self.host) + ":" + str(self.port))
+            self.socket.connect((self.host, port))
+            print("Connected to server at " + str(self.host) + ":" + str(port))
         except Exception as e:
             print("Error connecting to server: " + str(e))
             sys.exit(84)
 
+    # Send the data to the other player
     def send(self, data):
         try:
             if self.client_socket:
@@ -37,14 +40,16 @@ class Network:
             else:
                 self.socket.sendall(data.encode())
         except Exception as e:
-            print("Error sending data: " + e)
+            print("Error sending data: " + str(e))
     
+    # Receive data throught the socket
     def receive(self):
         if self.client_socket:
             return self.client_socket.recv(1024).decode()
         else:
             return self.socket.recv(1024).decode()
 
+    # Close connection
     def close_connection(self):
         if self.client_socket:
             self.client_socket.close()
